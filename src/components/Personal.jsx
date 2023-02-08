8974e6ec63e03ed042e7b23f61af1d18c55be3cb
codeboy0407 <ianm2927@gmail.com>
2023-02-08 15:43:43 -0500

    personal section style(mobile,tablet,desktop-done)

--- src/components/Personal.jsx
+++ src/components/Personal.jsx
@@ -3,6 +3,9 @@
 // import { UserContext } from "../context/UserContext";
 import Manimg from "../images/avatars/man_1.svg";
 function Personal({ data }) {
+  const deleteFamily = () => {
+    console.log("Delete Family btn");
+  };
   console.log(data);
   return (
     <div className="personal_container">
@@ -12,16 +15,40 @@ function Personal({ data }) {
             <picture>
               <img src={Manimg} alt="" />
             </picture>
-            <div>
-              <h2>Username:</h2>
-              <h2>{data.user.name}</h2>
-            </div>
-            <div>
-              <h2>ID:</h2>
-              <h2>{data.user._id}</h2>
+            <div className="metadata">
+              <div>
+                <h2>Username: {data.user.name}</h2>
+              </div>
+              <div>
+                <h2>ID: {data.user._id}</h2>
+              </div>
+              <div>
+                <h2>Role: Admi</h2>
+              </div>
             </div>
           </div>
           <div className="family_info">
+            <>
+              <div>
+                <h2>Family Name:</h2>
+                <h2>Los Torres</h2>
+              </div>
+              <div>
+                <h2>Family ID:</h2>
+                <h2>34343434343hb4kjk34k34</h2>
+              </div>
+              <div className="family_options">
+                <button
+                  style={{ background: "red" }}
+                  onClick={(e) => {
+                    deleteFamily();
+                  }}
+                >
+                  Delete Family
+                </button>
+                <button style={{ background: "green" }}>Rename Family</button>
+              </div>
+            </>
             {data.family ? (
               <>
                 <div>
@@ -34,11 +61,12 @@ function Personal({ data }) {
                 </div>
               </>
             ) : (
-              <div className="create_family_cont">
-                <button onClick={() => console.log("Create Family btn")}>
-                  Create family
-                </button>
-              </div>
+              // <div className="create_family_cont">
+              //   <button onClick={() => console.log("Create Family btn")}>
+              //     Create family
+              //   </button>
+              // </div>
+              ""
             )}
           </div>
         </>
