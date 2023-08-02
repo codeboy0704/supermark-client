e8e9463ee2e58686c7ae96ff973cb4502f0f7920
codeboy0407 <ian.flores2@utp.ac.pa>
2023-08-01 20:54:12 -0500

    product details data on page reload error(solved)

--- src/components/budget/cbasica/CanastaBasica.jsx
+++ src/components/budget/cbasica/CanastaBasica.jsx
@@ -5,46 +5,45 @@ import mockData from "./product.mock.data.json"
 import axios from 'axios'
 import LocationInfoContext from '../../../context/LocationInfoContext'
 
-function CanastaBasica({setProductDetails}) {
+function CanastaBasica({ setProductDetails }) {
     const locationInfo = useContext(LocationInfoContext)
-    const [products,setProducts] = useState([])
+    const [products, setProducts] = useState([])
     const [page, setPage] = useState(0)
     const [limit, setLimit] = useState(10)
     const [totalPages, setTotalPages] = useState(0)
-    const getProducts = async () =>{
-        const req = await axios.post('/api/product/get/many', {page: page, limit: limit })
+    const getProducts = async () => {
+        const req = await axios.post('/api/product/get/many', { page: page, limit: limit })
         const res = await req.data
         setProducts(res.data.data)
         setTotalPages(res.data.total)
     }
-    useEffect(()=>{
-      getProducts()
+    useEffect(() => {
+        getProducts()
     }, [page])
-    console.log(products)
-    
+
     return (
         <>
-        <MainLogo />
-        {locationInfo.latitude || locationInfo.longitude ? (
-        <div className='canasta_basica_cont'>
-        {products ? <ProductList arr={products} setProductDetails={setProductDetails} /> : <h1>Loading...</h1>}
-        <div className='page_options'>
-        <button onClick={()=>{
-            if(page !== 0){
-                setPage(page - 1)
-            }
-        }}>Previous</button>
-        <button onClick={()=>{
-            if(page < totalPages )
-                setPage(page + 1)
-        }}>Next</button>
-        </div>
-        </div>
-        ): (
-            <h1>Obteniendo ubicación...</h1>
-        )}
+            <MainLogo />
+            {locationInfo.latitude || locationInfo.longitude ? (
+                <div className='canasta_basica_cont'>
+                    {products ? <ProductList arr={products} setProductDetails={setProductDetails} /> : <h1>Loading...</h1>}
+                    <div className='page_options'>
+                        <button onClick={() => {
+                            if (page !== 0) {
+                                setPage(page - 1)
+                            }
+                        }}>Previous</button>
+                        <button onClick={() => {
+                            if (page < totalPages)
+                                setPage(page + 1)
+                        }}>Next</button>
+                    </div>
+                </div>
+            ) : (
+                <h1>Obteniendo ubicación...</h1>
+            )}
         </>
-  )
+    )
 }
 
 export default CanastaBasica
