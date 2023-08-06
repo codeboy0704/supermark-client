edd616190ea9c437693fbe8e75fb97e07955006e
codeboy0407 <ian.flores2@utp.ac.pa>
2023-08-06 15:51:08 -0500

    isLogin architecture added

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,16 +1,28 @@
 import "./home.css";
-import React, { useLayoutEffect } from "react";
+import React, { useContext, useLayoutEffect } from "react";
 import validation from "../../validations/signUp.validation";
 import Logo from "../../images/pre-logos/icons8-dollar-coin.svg";
 import FirstTime from "./FirstTime.jsx";
 import MainLogo from "../MainLogo";
+import { UserContext } from "../../context/UserContext";
+import Budget from "../budget/Budget";
 
 function Main() {
   const localStorage = window.localStorage;
+  const { user, login } = useContext(UserContext)
   return (
     <div className="home_container">
       <MainLogo />
-      <FirstTime />
+      {login ? (
+        <>
+          <Budget />
+        </>
+      ) : (
+        <>
+          <FirstTime />
+        </>
+
+      )}
     </div>
   );
 }
