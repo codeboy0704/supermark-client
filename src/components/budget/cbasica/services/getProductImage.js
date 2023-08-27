a0a9b557b0f50c98489a683e11bd909b8d7c0d10
codeboy0407 <ian.flores2@utp.ac.pa>
2023-08-27 13:39:05 -0500

    getImage function modified

--- src/components/budget/cbasica/services/getProductImage.js
+++ src/components/budget/cbasica/services/getProductImage.js
@@ -1,11 +1,12 @@
 import axios from "axios"
 
-const getProductImage = async (setImage) => {
+const getProductImage = async ({ setImage, id }) => {
     try {
-        const res = await axios.get('/api/image/sal')
-        const data = res.data.data.data
+        const res = await axios.get(`/api/image/${id}`)
+        const data = await res.data
+        const image = data.data.image.data
         const base64Img = btoa(
-            new Uint8Array(data).reduce((data, byte) => data + String.fromCharCode(byte), '')
+            new Uint8Array(image).reduce((data, byte) => data + String.fromCharCode(byte), '')
         )
         setImage(`data:${res.headers['content-type']};base64,${base64Img}`)
     } catch (e) {
