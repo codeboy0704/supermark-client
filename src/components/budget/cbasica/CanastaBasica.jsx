b8341615fd6e838b59c4309f7f07503345e63055
codeboy0407 <ian.flores2@utp.ac.pa>
2023-09-01 21:06:45 -0500

    LOading component added

--- src/components/budget/cbasica/CanastaBasica.jsx
+++ src/components/budget/cbasica/CanastaBasica.jsx
@@ -3,6 +3,7 @@ import MainLogo from '../../MainLogo'
 import ProductList from './ProductList'
 import axios from 'axios'
 import LocationInfoContext from '../../../context/LocationInfoContext'
+import Loading from '../../Loading'
 
 function CanastaBasica({ setProductDetails }) {
     const locationInfo = useContext(LocationInfoContext)
@@ -11,7 +12,7 @@ function CanastaBasica({ setProductDetails }) {
     const [limit, setLimit] = useState(10)
     const [totalPages, setTotalPages] = useState(0)
     const getProducts = async () => {
-        const req = await axios.post('/api/product/get/many', { page: page, limit: limit })
+        const req = await axios.get(`/api/product/many/${page}/${limit}`)
         const res = await req.data
         setProducts(res.data.data)
         setTotalPages(res.data.total)
@@ -22,24 +23,29 @@ function CanastaBasica({ setProductDetails }) {
 
     return (
         <>
-            <MainLogo />
             {locationInfo.latitude || locationInfo.longitude ? (
                 <div className='canasta_basica_cont'>
-                    {products ? <ProductList arr={products} setProductDetails={setProductDetails} /> : <h1>Loading...</h1>}
-                    <div className='page_options'>
-                        <button onClick={() => {
-                            if (page !== 0) {
-                                setPage(page - 1)
-                            }
-                        }}>Previous</button>
-                        <button onClick={() => {
-                            if (page < totalPages)
-                                setPage(page + 1)
-                        }}>Next</button>
-                    </div>
+                    {products.length > 1 ? (
+                        <>
+                            <ProductList arr={products} setProductDetails={setProductDetails} />
+                            <div className='page_options'>
+                                <button onClick={() => {
+                                    if (page !== 0) {
+                                        setPage(page - 1)
+                                    }
+                                }}>Previous</button>
+                                <button onClick={() => {
+                                    if (page < totalPages)
+                                        setPage(page + 1)
+                                }}>Next</button>
+                            </div>
+                        </>
+                    ) : (
+                        <Loading />
+                    )}
                 </div>
             ) : (
-                <h1>Obteniendo ubicación...</h1>
+                <Loading />
             )}
         </>
     )
