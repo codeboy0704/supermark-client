14fad36c11b24f3f38e598e525623a7123eb1bce
codeboy0407 <ian.flores2@utp.ac.pa>
2023-11-13 22:58:27 -0500

    get product image func request modify

--- src/components/budget/cbasica/services/getProductImage.js
+++ src/components/budget/cbasica/services/getProductImage.js
@@ -2,7 +2,13 @@ import axios from "axios"
 
 const getProductImage = async (id) => {
     try {
-        const res = await axios.get(`/api/image/${id}`, { responseType: 'arraybuffer' })
+        const res = await axios(`/api/image/${id}`, {
+            method: 'GET',
+            params: {
+                id: id
+            },
+            responseType: 'arraybuffer'
+        })
         const imageBlob = new Blob([res.data], { type: 'image/jpeg' });
         const imageUrl = URL.createObjectURL(imageBlob);
         return imageUrl;
